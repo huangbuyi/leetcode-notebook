5cc54c6ebe1f6fe562a868aaff75e058b7e289fc
huangsijs <huangsijs@gmail.com>
2018-04-13 17:51:49 +0800

    AC 238 260

---
+++ AC 238 Product of Array Except Self.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+  var left = 1, right = 1, res = new Array(nums.length)
+  for (var i = 0; i < nums.length; i++) {
+    res[i] = left
+    left *= nums[i]
+  }
+
+  for (var i = nums.length - 1; i >= 0; i--) {
+    res[i] *= right
+    right *= nums[i]
+  }
+
+  return res
+};
+
+/* 
+第一次遍历，保存遍历元素左边的乘积，第二次遍历，将左边的乘积乘以右边的乘积
+*/
+
+var assert = require('assert')
+assert.deepStrictEqual(productExceptSelf([1]), [1])
+assert.deepStrictEqual(productExceptSelf([2,2,2]), [4,4,4])
+assert.deepStrictEqual(productExceptSelf([1,2]), [2,1])
+assert.deepStrictEqual(productExceptSelf([1,2,3,4]), [24,12,8,6])
---
+++ AC 260 Single Number III.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+  var diff = 0, res = [0, 0]
+  for (var i = 0; i < nums.length; i++) {
+    diff ^= nums[i]
+  }
+  diff &= -diff
+  for (var i = 0; i < nums.length; i++) {
+    res[diff & nums[i] ? 0 : 1] ^= nums[i]
+  }
+  return res
+};
+
+/* 
+没想出来，看了答案。解题思路是进行两次异或遍历：
+第一次，得到的结果是两个 Single Number 的异或，因为这两个值必然是不同的，
+所以它们的异或结果的二进制值至少有一位为 1，找其中的一位（代码 diff &= -diiff 去最右边的一位 1）。例如：
+nums [1,2,1,3,2,5]
+3 0010
+5 0101
+diff 0110
+-diff 1010
+diff & -diff 0010
+1 位于右边第二位
+第二次，数组被分为两组进行异或，一组数右边第二位为 1，一组数右边第二位为 0，
+由于两个 Signle Number 数在右边第二位是不同的，所以这两个 Single Number 处于不同的组
+一组：[1,1,5] ([0001,0001,0101])
+二组：[2,2,3] ([0010,0010,0011])
+*/
