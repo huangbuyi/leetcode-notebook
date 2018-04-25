c0449ceec5457b2dda183d3dbf2114b936c68050
huangsijs <huangsijs@gmail.com>
2018-04-25 18:05:08 +0800

    AC 383 400

---
+++ AC 383 Ransom Note.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+  var hash = {}
+  for (var i = 0; i < magazine.length; i++) {
+    hash[magazine[i]] = (hash[magazine[i]] || 0) + 1
+  }
+  for (var i = 0; i < ransomNote.length; i++) {
+    if (!hash[ransomNote[i]]) {
+      return false
+    }
+    hash[ransomNote[i]]--
+  }
+  return true
+};
+
+var assert = require('assert')
+// example test
+assert.equal(canConstruct('a', 'b'), false)
+assert.equal(canConstruct('aa', 'ab'), false)
+assert.equal(canConstruct('aa', 'aab'), true)
+// more test
+assert.equal(canConstruct('', 'a'), true)
+assert.equal(canConstruct('a', ''), false)
+assert.equal(canConstruct('a', 'a'), true)
+assert.equal(canConstruct('aa', 'aa'), true)
+
---
+++ AC 400 Nth Digit.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+  var len = 1, count = 9
+  while (n > len * count) {
+    n -= len * count
+    len++
+    count *= 10
+  }
+  n--
+  return Number(String(Math.pow(10, len - 1) + Math.floor(n / len))[n % len])
+};
+
+/* 
+1-9 共9个数，长度1
+10-99 共90个数，长度2
+100-999 共900个数，长度3
+...
+*/
+
+var assert = require('assert')
+assert.equal(findNthDigit(1), 1)
+assert.equal(findNthDigit(9), 9)
+assert.equal(findNthDigit(10), 1)
+assert.equal(findNthDigit(11), 0)
+assert.equal(findNthDigit(1), 1)
+assert.equal(findNthDigit(15), 2)
+assert.equal(findNthDigit(10000), 7)
